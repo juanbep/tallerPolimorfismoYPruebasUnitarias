b1292c51b5e5f8ac4cdfcf4d9b1d4f7c0ef72500
juanbep <56483953+juanbep@users.noreply.github.com>
2021-02-15 18:17:11 -0500

    actualizando...

---
+++ src/main/java/co/edu/unicauca/tallerpolimorfismo/ViajeIncentivo.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unicauca.tallerpolimorfismo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Beca98
+ */
+public class ViajeIncentivo extends Viaje {
+
+    //Empresa que patrocina el viaje al empleado 
+    private String empresa;
+
+    public ViajeIncentivo(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada, String empresa) {
+        super(origen, destino, costo, fechaSalida, fechaLlegada);
+        this.empresa = empresa;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    @Override
+    public String descripcion() {
+        return "Viaje incentivo que te envia la empresa " + empresa;
+    }
+
+    @Override
+    public String cualquierMetodo2() {
+        return "Método implementado en la clase hija viaje de incentivo";
+    }
+
+}
